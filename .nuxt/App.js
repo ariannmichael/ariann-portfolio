9228c2fe0ea0bf1799797c2e6e80e80ba22c2f08
ariannfarias <ariannmichael@gmail.com>
2024-12-05 09:32:49 -0300

    Revert to 9c94d80

--- .nuxt/App.js
+++ .nuxt/App.js
@@ -4,6 +4,7 @@ import { decode, parsePath, withoutBase, withoutTrailingSlash, normalizeURL } fr
 import { getMatchedComponentsInstances, getChildrenComponentInstancesUsingFetch, promisify, globalHandleError, urlJoin, sanitizeComponent } from './utils'
 import NuxtError from './components/nuxt-error.vue'
 import NuxtLoading from './components/nuxt-loading.vue'
+import NuxtBuildIndicator from './components/nuxt-build-indicator'
 
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 
@@ -48,7 +49,7 @@ export default {
       }
     }, [
       loadingEl,
-
+      h(NuxtBuildIndicator),
       transitionEl
     ])
   },
@@ -124,12 +125,20 @@ export default {
       }
       this.$loading.start()
 
-      const promises = pages.map(async (page) => {
-        let p = []
+      const promises = pages.map((page) => {
+        const p = []
 
         // Old fetch
         if (page.$options.fetch && page.$options.fetch.length) {
           p.push(promisify(page.$options.fetch, this.context))
+        }
+        if (page.$fetch) {
+          p.push(page.$fetch())
+        } else {
+          // Get all component instance to call $fetch
+          for (const component of getChildrenComponentInstancesUsingFetch(page.$vnode.componentInstance)) {
+            p.push(component.$fetch())
+          }
         }
 
         if (page.$options.asyncData) {
@@ -141,19 +150,6 @@ export default {
                 }
               })
           )
-        }
-
-        // Wait for asyncData & old fetch to finish
-        await Promise.all(p)
-        // Cleanup refs
-        p = []
-
-        if (page.$fetch) {
-          p.push(page.$fetch())
-        }
-        // Get all component instance to call $fetch
-        for (const component of getChildrenComponentInstancesUsingFetch(page.$vnode.componentInstance)) {
-          p.push(component.$fetch())
         }
 
         return Promise.all(p)
@@ -189,6 +185,10 @@ export default {
     },
 
     setLayout (layout) {
+      if(layout && typeof layout !== 'string') {
+        throw new Error('[nuxt] Avoid using non-string value as layout property.')
+      }
+
       if (!layout || !layouts['_' + layout]) {
         layout = 'default'
       }
